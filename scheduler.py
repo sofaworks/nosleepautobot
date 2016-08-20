ffb8642ef7c26c820498088976253a5c7b4c4d4e
Sofa Assassin <sofaassassin@users.noreply.github.com>
2016-08-20 16:33:24 -0400

    Removing the procfile/scheduler again to revert back to running the bot via Heroku scheduler

--- scheduler.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-sched = BlockingScheduler()
-
-@sched.scheduled_job('interval', minutes=3)
-def timed_job():
-    print("Running the bot every 3 minutes.")
