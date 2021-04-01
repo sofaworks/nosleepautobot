cef47b09cf57106db3f8c0d7ae3bf4f6cc365c23
William Lee <wkl@fb.com>
2021-03-31 21:36:07 -0400

    Handle rate limits when sending mod reports

--- tools/track_mod_activity.py
+++ tools/track_mod_activity.py
@@ -1,20 +1,37 @@
-#!/usr/bin/env python
-
-
+#!/usr/bin/env python3
 
 from collections import defaultdict
 import os
+import re
 import sys
-import time
 import logging
 import datetime
-import itertools
+import time
 
 import praw
-from praw.models.reddit.subreddit import SubredditModeration
+from praw.exceptions import RedditAPIException
 
 
 MOD_ACTIONS = ['approvelink', 'removelink', 'approvecomment', 'removecomment']
+
+_pattern = re.compile(r'(?P<number>[0-9]+) (?P<unit>\w+)s? before trying.*\.$',
+                      re.IGNORECASE)
+
+
+def handle_rate_limit(exc: RedditAPIException) -> None:
+    time_map = {
+        'seconds': 1,
+        'minutes': 60,
+        'hours': 60 * 60,
+    }
+    matches = re.search(_pattern, exc.message)
+    if not matches:
+        logging.error(f'Unable to parse rate limit message {exc.message!r}')
+        return
+    logging.info(f"Matched: {matches}")
+    delay = int(matches['number']) * time_map[matches['unit']]
+    time.sleep(delay + 1)
+
 
 if __name__ == '__main__':
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
@@ -28,17 +45,16 @@ if __name__ == '__main__':
     subreddit = reddit.subreddit(os.environ['AUTOBOT_SUBREDDIT'])
 
     today = datetime.datetime.utcnow()
-    
+
     # Run every Friday - since this thing runs in Heroku, which has
     # very inflexible scheduling (best is once-per-day), this will be enforced
     # here
     if today.weekday() != 4:
         print("Not running mod activity tracker because it's not Friday")
         sys.exit(0)
-        
+
     month_start = datetime.datetime(today.year, today.month, 1)
     start_ts = time.mktime(month_start.timetuple())
-
 
     if not subreddit.user_is_moderator:
         raise AssertionError("User is not moderator of subreddit")
@@ -51,6 +67,8 @@ if __name__ == '__main__':
         if moderator.name.lower() in ['nosleepautobot', 'automoderator']:
             print("Skipping {}".format(moderator))
             continue
+        else:
+            print("Generating report for {}".format(moderator))
 
         action_days = set()
         summary = defaultdict(int)
@@ -82,5 +100,22 @@ Friendly reminder to meet your minimums for the month!
         summary['removecomment'],
         len(action_days))
         print(message)
-        
-        moderator.message("r/nosleep moderation minimum activity reminder", message)
+
+        print(reddit.auth.limits)
+        while True:
+            try:
+                moderator.message("r/nosleep moderation minimum activity reminder", message)
+            except RedditAPIException as e:
+                rate_limited = False
+                for subex in e.items:
+                    if subex.error_type == "RATELIMIT":
+                        rate_limited = True
+                        logging.warning(
+                            "Ratelimit - artificially limited by Reddit. Sleeping for requested time!"
+                        )
+                        handle_rate_limit(e)
+                if rate_limited:
+                    continue
+                else:
+                    raise e
+            break
